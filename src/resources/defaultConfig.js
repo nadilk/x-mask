ede2f74bfc0e754b11bfce2546667310e3de7840
nadilk <nadilk@yandex.ru>
2021-01-05 09:32:36 +0600

    Update defaultConfig.js
    fixed regex for latin letters

--- src/resources/defaultConfig.js
+++ src/resources/defaultConfig.js
@@ -3,7 +3,7 @@ export default {
     symbols: [
         {alias: '*', type: 'regex', value: /./},
         {alias: '#', type: 'regex', value: /[0-9]/},
-        {alias: 'A', type: 'regex', value: /[a-zA-z]/},
+        {alias: 'A', type: 'regex', value: /[a-zA-Z]/},
         {alias: 'U', type: 'function', value: ({symbol,index,xmask},mode) => {
             if(mode === 'write')
                 xmask.writeSymbolAt(symbol.toUpperCase(),index)
